20d1112c596a0c54138be0093f325d42ccdb6e1d
wangfeiyang <knowthat@qq.com>
2024-03-17 22:27:09 +0800

    update some

--- app/Enums/WebsocketMessageTypeEnum.php
+++ app/Enums/WebsocketMessageTypeEnum.php
@@ -10,5 +10,5 @@ enum WebsocketMessageTypeEnum: string
     use CollectTrait;
 
     case ChatSession = 'chat_session'; // 会话列表
-    case Chat = 'chat'; // 聊天
+    case Chat = 'chat_single'; // 聊天
 }
